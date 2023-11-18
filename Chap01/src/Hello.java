a061e985110da83c13ae37b00bbbe9bcb053fddf
leoaug590 <kcc6950@naver.com>
2023-11-19 01:34:03 +0900

    upload

--- Chap01/src/Hello.java
+++ Chap01/src/Hello.java
@@ -2,26 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		byte a1 = 127;
-		System.out.println(a1);
-		a1++;
-		System.out.println(a1);
-		a1++;
-		System.out.println(a1);
-		a1++;
-		System.out.println(a1);
-		// a = null;
-		
-		// 줄 주석
-		/* 블록 주석 */
-		
-		
-		int a = 5;
-		int b = 3;
-		float c = a / b;
-		System.out.println(c);
-		
+		System.out.println("Hello JAVA!");
 	}
 
 }
--- Chap06/src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-	String datas = "Java, Book";
-	String data[] = datas.split(",");
-	System.out.println(data[0]);
-    }
-}
