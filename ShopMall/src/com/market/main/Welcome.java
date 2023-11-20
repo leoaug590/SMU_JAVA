3ca9ff0d37e5d6bbf3063b8430fc5855b8b123b3
leoaug590 <kcc6950@naver.com>
2023-11-20 19:37:08 +0900

    REFACTOR

--- ShopMall/src/com/market/main/Welcome.java
+++ ShopMall/src/com/market/main/Welcome.java
@@ -101,11 +101,11 @@ public class Welcome {
 
 	public static void menuIntroduction() {
 		System.out.println("***********************************************");
-		System.out.println("1. 고객 정보 확인하기\t4. 바구니에 항목 추가하기");
-		System.out.println("2. 장바구니 상품 목록보기\t5. 장바구니의 항목 수량 줄이기");
-		System.out.println("3. 장바구니 비우기	\t6. 장바구니의 항목 삭제하기");
-		System.out.println("7. 영수증 표시하기	\t8. 종료");
-		System.out.println("9. 관리자 로그인");
+		System.out.println("1. 고객 정보 확인하기\t6. 장바구니의 항목 삭제하기");
+		System.out.println("2. 장바구니 상품 목록보기\t7. 영수증 표시하기");
+		System.out.println("3. 장바구니 비우기	\t8. 종료");
+		System.out.println("4. 바구니에 항목 추가하기\t9. 관리자 로그인");
+		System.out.println("5. 장바구니의 항목 수량 줄이기");
 		System.out.println("***********************************************");
 	}
 
