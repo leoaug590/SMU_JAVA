8c336597cc8db364efff4bb5e7c3ad3c3f7e50df
leoaug590 <kcc6950@naver.com>
2023-11-24 18:20:35 +0900

    UPLOAD

--- Chap08/src/Example06.java
+++ Chap08/src/Example06.java
@@ -3,7 +3,7 @@ public class Example06 {
 
 	public static void main(String[] args) {
 		SubKitten2 obj = new SubKitten2("아름이", "다운이");
-
+		obj.name = " ";
 	}
 
 }
--- Chap08/src/Inheritance06.java
+++ Chap08/src/Inheritance06.java
@@ -3,7 +3,7 @@ public class Inheritance06 {
 
 	public static void main(String[] args) {
 		Child2 objChild2 = new Child2();
-
+		objChild2.name = " ";
 	}
 
 }
